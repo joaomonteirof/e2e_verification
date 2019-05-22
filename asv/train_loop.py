fix on normalization

asv/train_loop.py
@@ -176,7 +176,7 @@ class TrainLoop(object):
 
 		embeddings = self.model.forward(utterances)
 
-		embeddings_norm = torch.div(embeddings, torch.norm(embeddings, 2, 1).unsqueeze(1).expand_as(embeddings))
+		embeddings_norm = F.normalize(embeddings, p=2, dim=1)
 
 		ce_loss = F.cross_entropy(self.model.out_proj(embeddings_norm, y), y)
 
@@ -221,11 +221,13 @@ class TrainLoop(object):
 		utt = utt[:,:,:,:ridx]
 
 		if self.cuda_mode:
-			utt, y = utt.cuda(self.device), y.cuda(self.device)
+			utt, y = utt.cuda(self.device), y.cuda(self.device).squeeze()
 
 		embeddings = self.model.forward(utt)
 
-		loss = F.cross_entropy(self.model.out_proj(embeddings), y.squeeze())
+		embeddings_norm = F.normalize(embeddings, p=2, dim=1)
+
+		loss = F.cross_entropy(self.model.out_proj(embeddings_norm, y), y)
 
 		loss.backward()
 		self.optimizer.step()

asv/utils/losses.py
@@ -2,6 +2,7 @@ import math
 import torch
 from torch import nn
 from scipy.special import binom
+import torch.nn.functional as F
 
 class AMSoftmax(nn.Module):
 
@@ -27,7 +28,7 @@ class AMSoftmax(nn.Module):
 
 		self.w.to(embeddings.device)
 
-		w_norm = torch.div(self.w, torch.norm(self.w, 2, 0))
+		w_norm = F.normalize(self.w, p=2, dim=0)
 
 		cos_theta = embeddings.mm(self.w)
 		cos_theta = torch.clamp(cos_theta, -1.0, 1.0)

cifar/train_loop.py
@@ -170,7 +170,7 @@ class TrainLoop(object):
 
 		embeddings = self.model.forward(x)
 
-		embeddings_norm = torch.div(embeddings, torch.norm(embeddings, 2, 1).unsqueeze(1).expand_as(embeddings))
+		embeddings_norm = F.normalize(embeddings, p=2, dim=1)
 
 		ce_loss = F.cross_entropy(self.model.out_proj(embeddings_norm, y), y)
 
@@ -207,11 +207,12 @@ class TrainLoop(object):
 
 		x, y = batch
 
-		x, y = x.to(self.device), y.to(self.device)
+		x, y = x.to(self.device), y.to(self.device).squeeze()
 
 		embeddings = self.model.forward(utt)
+		embeddings_norm = F.normalize(embeddings, p=2, dim=1)
 
-		loss = F.cross_entropy(self.model.out_proj(embeddings), y.squeeze())
+		loss = F.cross_entropy(self.model.out_proj(embeddings_norm, y), y)
 
 		loss.backward()
 		self.optimizer.step()
@@ -231,7 +232,7 @@ class TrainLoop(object):
 
 			embeddings = self.model.forward(x)
 
-			embeddings_norm = torch.div(embeddings, torch.norm(embeddings, 2, 1).unsqueeze(1).expand_as(embeddings))
+			embeddings_norm = F.normalize(embeddings, p=2, dim=1)
 
 			ce_loss = F.cross_entropy(self.model.out_proj(embeddings_norm, y), y)
 