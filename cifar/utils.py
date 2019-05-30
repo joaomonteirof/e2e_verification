98c2eef867d00b8dc4f5ae709614fb6fe802c607
joaomonteirof <joaomonteirof@gmail.com>
2019-05-30 12:30:31 -0400

    minor fix

--- cifar/utils.py
+++ cifar/utils.py
@@ -16,7 +16,6 @@ def get_freer_gpu(trials=10):
 	for j in range(trials):
 		os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
 		memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
-		os.remove('tmp')
 		dev_ = torch.device('cuda:'+str(np.argmax(memory_available)))
 		try:
 			a = torch.rand(1).cuda(dev_)
