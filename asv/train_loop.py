c7a959920d95fa8a3b48064538c0c314f922220d
joaomonteirof <joaomonteirof@gmail.com>
2019-08-16 11:36:46 -0400

    label smoothing on D

--- asv/train_loop.py
+++ asv/train_loop.py
@@ -235,7 +235,7 @@ class TrainLoop(object):
 		emb_an = torch.cat([emb_a, emb_n],1)
 		emb_ = torch.cat([emb_ap, emb_an],0)
 
-		y_ = torch.cat([torch.ones(emb_ap.size(0)), torch.zeros(emb_an.size(0))],0)
+		y_ = torch.cat([torch.rand(emb_ap.size(0))*0.2+0.8, torch.rand(emb_an.size(0))*0.2],0) if isinstance(self.ce_criterion, LabelSmoothingLoss) else torch.cat([torch.ones(emb_ap.size(0)), torch.zeros(emb_an.size(0))],0)
 
 		if self.cuda_mode:
 			y_ = y_.cuda(self.device)
