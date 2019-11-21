fix on random projection normalization

asv/model.py
@@ -816,16 +816,11 @@ class TDNN(nn.Module):
 	def make_bin_layers(self, n_in, n_h_layers, h_size, dropout_p):
 
 		if self.r_proj_size>0:
-#			projection = nn.Linear(n_in, self.r_proj_size, bias=False)
-#			with torch.no_grad():
-#				projection.weight.div_(torch.norm(projection.weight, keepdim=True))
-
-			projection = nn.utils.weight_norm(nn.Linear(n_in, self.r_proj_size, bias=False))
+			projection = nn.Linear(n_in, self.r_proj_size, bias=False)
 			with torch.no_grad():
-				projection.weight_g.fill_(1)
+				projection.weight /= torch.norm(projection.weight.squeeze()).item()
 
 			projection.weight.require_grad=False
-			projection.weight_g.require_grad=False
 
 			classifier = nn.ModuleList([projection, nn.Linear(self.r_proj_size, h_size), nn.LeakyReLU(0.1)])
 