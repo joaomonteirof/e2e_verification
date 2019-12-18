db07e90371485389ac47fdf26c06a3157bfffe49
joaomonteirof <joaomonteirof@gmail.com>
2019-12-17 19:14:04 -0500

    added eval all for mini imagenet

--- cifar/eval_acc_all.py
+++ cifar/eval_acc_all.py
@@ -37,7 +37,8 @@ if __name__ == '__main__':
 		ckpt = torch.load(cp, map_location = lambda storage, loc: storage)
 		try :
 			dropout_prob, n_hidden, hidden_size, softmax = ckpt['dropout_prob'], ckpt['n_hidden'], ckpt['hidden_size'], ckpt['sm_type']
-		except KeyError:
+		except KeyError as err:
+			print("Key Error: {0}".format(err))
 			print('\nProbably old cp has no info regarding classifiers arch!\n')
 			n_hidden, hidden_size, softmax = get_classifier_config_from_cp(ckpt)
 			dropout_prob = args.dropout_prob
