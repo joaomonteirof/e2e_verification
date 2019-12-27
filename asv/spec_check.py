80eb7dc9f2a4fb8d6599b04d4792c5b7454382f3
joaomonteirof <joaomonteirof@gmail.com>
2019-12-27 17:31:14 -0500

    added scripts to evaluate the min eigenvalue of the learned dist matrices

---
+++ asv/spec_check.py
@@ -0,0 +1,175 @@
+import argparse
+import numpy as np
+from numpy import linalg as LA
+import torch
+from kaldi_io import read_mat_scp
+from sklearn import metrics
+import scipy.io as sio
+import model as model_
+import glob
+import pickle
+import os
+import sys
+import itertools
+from tqdm import tqdm
+from utils.utils import *
+import random
+
+def prep_feats(data_, min_nb_frames=100):
+
+	features = data_.T
+
+	if features.shape[1]<min_nb_frames:
+		mul = int(np.ceil(min_nb_frames/features.shape[1]))
+		features = np.tile(features, (1, mul))
+		features = features[:, :min_nb_frames]
+
+	return torch.from_numpy(features[np.newaxis, np.newaxis, :, :]).float()
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='Evaluation')
+	parser.add_argument('--test-data', type=str, default='./data/test/', metavar='Path', help='Path to input data')
+	parser.add_argument('--trials-path', type=str, default=None, help='Path to trials file. If None, will be created from spk2utt')
+	parser.add_argument('--spk2utt', type=str, default=None, metavar='Path', help='Path to spk2utt file. Will be used in case no trials file is provided')
+	parser.add_argument('--cp-path', type=str, default=None, metavar='Path', help='Path for file containing model')
+	parser.add_argument('--model', choices=['resnet_stats', 'resnet_mfcc', 'resnet_lstm', 'resnet_small', 'resnet_large', 'TDNN'], default='resnet_lstm', help='Model arch according to input type')
+	parser.add_argument('--sample-size', type=int, default=100, metavar='N', help='Sample size (default: 100)')
+	parser.add_argument('--n-repeat', type=int, default=1, metavar='N', help='Number of repetitions')
+	parser.add_argument('--out-path', type=str, default='./', metavar='Path', help='Path for saving outputs')
+	parser.add_argument('--out-prefix', type=str, default=None, metavar='Path', help='Prefix to be added to output file name')
+	parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables GPU use')
+	parser.add_argument('--inner', action='store_true', default=True, help='Inner layer as embedding')
+	parser.add_argument('--no-histogram', action='store_true', default=False, help='Disables histogram plot')
+	args = parser.parse_args()
+	args.cuda = True if not args.no_cuda and torch.cuda.is_available() else False
+
+	if args.cp_path is None:
+		raise ValueError('There is no checkpoint/model path. Use arg --cp-path to indicate the path!')
+
+	print('Cuda Mode is: {}'.format(args.cuda))
+
+	if args.cuda:
+		device = get_freer_gpu()
+
+	ckpt = torch.load(args.cp_path, map_location = lambda storage, loc: storage)
+
+	if args.model == 'resnet_mfcc':
+		model = model_.ResNet_mfcc(n_z=ckpt['latent_size'], nh=ckpt['n_hidden'], n_h=ckpt['hidden_size'], proj_size=ckpt['r_proj_size'], ncoef=ckpt['ncoef'], ndiscriminators=ckpt['ndiscriminators'])
+	elif args.model == 'resnet_lstm':
+		model = model_.ResNet_lstm(n_z=ckpt['latent_size'], nh=ckpt['n_hidden'], n_h=ckpt['hidden_size'], proj_size=ckpt['r_proj_size'], ncoef=ckpt['ncoef'], ndiscriminators=ckpt['ndiscriminators'])
+	elif args.model == 'resnet_stats':
+		model = model_.ResNet_stats(n_z=ckpt['latent_size'], nh=ckpt['n_hidden'], n_h=ckpt['hidden_size'], proj_size=ckpt['r_proj_size'], ncoef=ckpt['ncoef'], ndiscriminators=ckpt['ndiscriminators'])
+	elif args.model == 'resnet_small':
+		model = model_.ResNet_small(n_z=ckpt['latent_size'], nh=ckpt['n_hidden'], n_h=ckpt['hidden_size'], proj_size=ckpt['r_proj_size'], ncoef=ckpt['ncoef'], ndiscriminators=ckpt['ndiscriminators'])
+	elif args.model == 'resnet_large':
+		model = model_.ResNet_large(n_z=ckpt['latent_size'], nh=ckpt['n_hidden'], n_h=ckpt['hidden_size'], proj_size=ckpt['r_proj_size'], ncoef=ckpt['ncoef'], ndiscriminators=ckpt['ndiscriminators'])
+	elif args.model == 'TDNN':
+		model = model_.TDNN(n_z=ckpt['latent_size'], nh=ckpt['n_hidden'], n_h=ckpt['hidden_size'], proj_size=ckpt['r_proj_size'], ncoef=ckpt['ncoef'], ndiscriminators=ckpt['ndiscriminators'])
+
+
+	try:
+		model.load_state_dict(ckpt['model_state'], strict=True)
+	except RuntimeError as err:
+		print("Runtime Error: {0}".format(err))
+	except:
+		print("Unexpected error:", sys.exc_info()[0])
+		raise
+
+	model.eval()
+	if args.cuda:
+		model = model.to(device)
+
+	test_data = None
+
+	files_list = glob.glob(args.test_data+'*.scp')
+
+	for file_ in files_list:
+		if test_data is None:
+			test_data = { k:v for k,v in read_mat_scp(file_) }
+		else:
+			for k,v in read_mat_scp(file_):
+				test_data[k] = v
+
+	if args.trials_path:
+		_, utterances_list, _ = read_trials(args.trials_path)
+		utterances_list = np.unique(utterances_list)
+	else:
+		spk2utt = read_spk2utt(args.spk2utt)
+		utterances_list = []
+		for spk in spk2utt:
+			utterances_list.extend(spk2utt[spk])
+
+	print('\nAll data ready. Start of scoring')
+
+	min_eig_list = []
+
+	for k in range(args.n_repeat):
+
+		idx_list = random.sample(range(len(utterances_list)), min(len(utterances_list), args.sample_size))
+		scores_mat = np.zeros((args.sample_size, args.sample_size))
+		mem_embeddings = {}
+
+		model.eval()
+
+		with torch.no_grad():
+
+			print('\nPreparing distance dictionary.')
+
+			pairs = itertools.permutations(range(len(idx_list)), 2)
+			iterator = tqdm(pairs, total=len(idx_list)**2)
+
+			for i, j in iterator:
+
+				anchor_utt = str(i)
+
+				try:
+					emb_anchor = mem_embeddings[anchor_utt]
+				except KeyError:
+
+					anchor_utt_data = prep_feats(test_data[utterances_list[idx_list[i]]])
+
+					if args.cuda:
+						anchor_utt_data = anchor_utt_data.to(device)
+
+					emb_anchor = model.forward(anchor_utt_data)[1].detach() if args.inner else model.forward(anchor_utt_data)[0].detach()
+					mem_embeddings[anchor_utt] = emb_anchor
+
+
+				a_utt = str(j)
+
+				try:
+					emb_a = mem_embeddings[a_utt]
+				except KeyError:
+
+					a_utt_data = prep_feats(test_data[utterances_list[idx_list[j]]])
+
+					if args.cuda:
+						a_utt_data = a_utt_data.to(device)
+
+					emb_a = model.forward(a_utt_data)[1].detach() if args.inner else model.forward(a_utt_data)[0].detach()
+					mem_embeddings[a_utt] = emb_a
+
+				scores_mat[i,j] = model.forward_bin(torch.cat([emb_anchor, emb_a],1)).squeeze().item()
+
+			print('\nComputing scores differences.')
+
+			w, v = LA.eig(scores_mat)
+			min_eig_list.append(np.min(w))
+			print('\nMinimum eigenvalue: {}'.format(min_eig_list[-1]))
+
+	print('\nScoring done')
+
+	if args.n_repeat>2:
+		print('Avg: {}'.format(np.mean(min_eig_list)))
+		print('Std: {}'.format(np.std(min_eig_list)))
+		print('Median: {}'.format(np.median(min_eig_list)))
+		print('Max: {}'.format(np.max(min_eig_list)))
+		print('Min: {}'.format(np.min(min_eig_list)))
+
+	if not args.no_histogram:
+		import matplotlib
+		matplotlib.use('agg')
+		import matplotlib.pyplot as plt
+		plt.hist(w, density=True, bins=30)
+		plt.savefig(args.out_path+args.out_prefix+'spec_vox.pdf', bbox_inches='tight')
---
+++ cifar/spec_check.py
@@ -0,0 +1,133 @@
+from __future__ import print_function
+import argparse
+import torch
+from train_loop import TrainLoop
+import torch.optim as optim
+from torchvision import datasets, transforms
+from models import vgg, resnet, densenet
+import numpy as np
+from numpy import linalg as LA
+import os
+import sys
+import itertools
+from tqdm import tqdm
+from utils import *
+import random
+
+if __name__ == '__main__':
+
+
+	parser = argparse.ArgumentParser(description='Symmetry check')
+	parser.add_argument('--cp-path', type=str, default=None, metavar='Path', help='Path for checkpointing')
+	parser.add_argument('--data-path', type=str, default='./data/', metavar='Path', help='Path to data')
+	parser.add_argument('--model', choices=['vgg', 'resnet', 'densenet'], default='resnet')
+	parser.add_argument('--sample-size', type=int, default=100, metavar='N', help='Sample size (default: 100)')
+	parser.add_argument('--n-repeat', type=int, default=1, metavar='N', help='Number of repetitions')
+	parser.add_argument('--dropout-prob', type=float, default=0.25, metavar='p', help='Dropout probability (default: 0.25)')
+	parser.add_argument('--out-path', type=str, default=None, metavar='Path', help='Path for saving computed scores')
+	parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables GPU use')
+	parser.add_argument('--no-histogram', action='store_true', default=False, help='Disables histogram plot')
+	args = parser.parse_args()
+	args.cuda = True if not args.no_cuda and torch.cuda.is_available() else False
+
+	transform_test = transforms.Compose([transforms.ToTensor(), transforms.Normalize([x / 255 for x in [125.3, 123.0, 113.9]], [x / 255 for x in [63.0, 62.1, 66.7]])])
+	validset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+
+	ckpt = torch.load(args.cp_path, map_location = lambda storage, loc: storage)
+	try :
+		dropout_prob, n_hidden, hidden_size, softmax = ckpt['dropout_prob'], ckpt['n_hidden'], ckpt['hidden_size'], ckpt['sm_type']
+	except KeyError:
+		print('\nProbably old cp has no info regarding classifiers arch!\n')
+		n_hidden, hidden_size, softmax = get_classifier_config_from_cp(ckpt)
+		dropout_prob = args.dropout_prob
+
+	if args.model == 'vgg':
+		model = vgg.VGG('VGG16', nh=n_hidden, n_h=hidden_size, dropout_prob=dropout_prob, sm_type=softmax)
+	elif args.model == 'resnet':
+		model = resnet.ResNet18(nh=n_hidden, n_h=hidden_size, dropout_prob=dropout_prob, sm_type=softmax)
+	elif args.model == 'densenet':
+		model = densenet.densenet_cifar(nh=n_hidden, n_h=hidden_size, dropout_prob=dropout_prob, sm_type=softmax)
+	
+	try:
+		model.load_state_dict(ckpt['model_state'], strict=True)
+	except RuntimeError as err:
+		print("Runtime Error: {0}".format(err))
+	except:
+		print("Unexpected error:", sys.exc_info()[0])
+		raise
+
+	if args.cuda:
+		device = get_freer_gpu()
+		model = model.cuda(device)
+
+	min_eig_list = []
+
+	for k in range(args.n_repeat):
+
+		scores_mat = np.zeros((args.sample_size, args.sample_size))
+		idx_list = random.sample(range(len(validset)), min(len(validset), args.sample_size))
+		mem_embeddings = {}
+
+		model.eval()
+
+		with torch.no_grad():
+
+			print('\nPreparing distance dictionary.')
+
+			pairs = itertools.permutations(range(len(idx_list)), 2)
+			iterator = tqdm(pairs, total=len(idx_list)**2)
+
+			for i, j in iterator:
+
+				anchor_ex = str(i)
+
+				try:
+					emb_anchor = mem_embeddings[anchor_ex]
+				except KeyError:
+
+					anchor_ex_data = validset[idx_list[i]][0].unsqueeze(0)
+
+					if args.cuda:
+						anchor_ex_data = anchor_ex_data.cuda(device)
+
+					emb_anchor = model.forward(anchor_ex_data).detach()
+					mem_embeddings[anchor_ex] = emb_anchor
+
+				a_ex = str(j)
+
+				try:
+					emb_a = mem_embeddings[a_ex]
+				except KeyError:
+
+					a_ex_data = validset[idx_list[j]][0].unsqueeze(0)
+
+					if args.cuda:
+						a_ex_data = a_ex_data.cuda(device)
+
+					emb_a = model.forward(a_ex_data).detach()
+					mem_embeddings[a_ex] = emb_a
+
+				scores_mat[i,j] = model.forward_bin(torch.cat([emb_anchor, emb_a],1)).squeeze().item()
+
+
+			print('\nComputing spectrum.')
+
+			w, v = LA.eig(scores_mat)
+			min_eig_list.append(np.min(w))
+			print('\nMinimum eigenvalue: {}'.format(min_eig_list[-1]))
+
+	print('\nScoring done.')
+
+	if args.n_repeat>2:
+		print('Avg: {}'.format(np.mean(min_eig_list)))
+		print('Std: {}'.format(np.std(min_eig_list)))
+		print('Median: {}'.format(np.median(min_eig_list)))
+		print('Max: {}'.format(np.max(min_eig_list)))
+		print('Min: {}'.format(np.min(min_eig_list)))
+
+	if not args.no_histogram:
+		import matplotlib
+		matplotlib.use('agg')
+		import matplotlib.pyplot as plt
+		plt.hist(w, density=True, bins=30)
+		plt.savefig(args.out_path+args.out_prefix+'spec_hist_cifar.pdf', bbox_inches='tight')
