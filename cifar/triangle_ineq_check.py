dfdd66dff26e34870dee6baf4b5918d34d40726a
joaomonteirof <joaomonteirof@gmail.com>
2019-12-29 19:10:23 -0500

    verbosity

--- cifar/triangle_ineq_check.py
+++ cifar/triangle_ineq_check.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
 	parser.add_argument('--sample-size', type=int, default=5000, metavar='N', help='Sample size (default: 5000)')
 	parser.add_argument('--dropout-prob', type=float, default=0.25, metavar='p', help='Dropout probability (default: 0.25)')
 	parser.add_argument('--out-path', type=str, default=None, metavar='Path', help='Path for saving computed scores')
+	parser.add_argument('--out-prefix', type=str, default=None, metavar='Path', help='Prefix to be added to output file name')
 	parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables GPU use')
 	parser.add_argument('--no-histogram', action='store_true', default=False, help='Disables histogram plot')
 	args = parser.parse_args()
