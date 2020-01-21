5c8ecc903c386fe136003ffb8c2ff406d69968c7
Joao Monteiro <joaomonteirof@Joaos-MacBook-Pro.local>
2020-01-20 16:48:30 -0800

    adding new sampling scheme

--- mini_imagenet/data_prep.py
+++ mini_imagenet/data_prep.py
@@ -13,14 +13,14 @@ if __name__ == '__main__':
 	parser.add_argument('--path-to-data', type=str, default='./data/', metavar='Path', help='Path to scp files with features')
 	parser.add_argument('--out-path', type=str, default='./', metavar='Path', help='Path to output hdf file')
 	parser.add_argument('--out-name', type=str, default='train.hdf', metavar='Path', help='Output hdf file name')
-	parser.add_argument('--min-examples', type=int, default=-1, help='Minimum number of examples per class')
 	args = parser.parse_args()
 
 	if os.path.isfile(args.out_path+args.out_name):
 		os.remove(args.out_path+args.out_name)
 		print(args.out_path+args.out_name+' Removed')
 
-	dataset = datasets.ImageFolder(args.data_path, transform=transforms)
+	transform = transforms.Compose([transforms.ToTensor()])
+	dataset = datasets.ImageFolder(args.data_path, transform=transform)
 	dataloader = torch.utils.data.DataLoader(trainset, batch_size=1, shuffle=False, num_workers=args.n_workers)
 
 	print('Start of data preparation')
@@ -37,6 +37,6 @@ if __name__ == '__main__':
 		if not y in hdf:
 			hdf.create_group(spk)
 
-		hdf[y].create_dataset(y+'_'+str(i), data=features)
+		hdf[y].create_dataset(y+'_'+str(i), data=x.squeeze(0))
 
 	hdf.close()
