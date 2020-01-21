af326179fe43400ccc80520823460f26221662df
Joao Monteiro <joaomonteirof@Joaos-MacBook-Pro.local>
2020-01-20 16:42:21 -0800

    adding new sampling scheme

---
+++ mini_imagenet/data_load.py
@@ -0,0 +1,98 @@
+import h5py
+import numpy as np
+import glob
+import torch
+from torch.utils.data import Dataset
+import os
+import subprocess
+import shlex
+from utils.utils import strided_app
+
+class Loader(Dataset):
+
+	def __init__(self, hdf5_name, transformation):
+		super(Loader, self).__init__()
+		self.hdf5_name = hdf5_name
+		self.transformation = transformation
+
+		self.create_lists()
+
+		self.open_file = None
+
+		self.update_lists()
+
+	def __getitem__(self, index):
+
+		example_1, example_2, example_3, example_4, example_5, clss, y = self.example_list[index]
+
+		if not self.open_file: self.open_file = h5py.File(self.hdf5_name, 'r')
+
+		example_1_data = self.transformation( self.open_file[clss][example_1] )
+		example_2_data = self.transformation( self.open_file[clss][example_2] )
+		example_3_data = self.transformation( self.open_file[clss][example_3] )
+		example_4_data = self.transformation( self.open_file[clss][example_4] )
+		example_5_data = self.transformation( self.open_file[clss][example_5] )
+
+		return example_1_data.contiguous(), example_2_data.contiguous(), example_3_data.contiguous(), example_4_data.contiguous(), example_5_data.contiguous(), y
+
+	def __len__(self):
+		return len(self.example_list)
+
+	def create_lists(self):
+
+		open_file = h5py.File(self.hdf5_name, 'r')
+
+		self.class2file = {}
+		self.clss2label = {}
+
+		for i, clss in enumerate(open_file):
+			clss_example_list = list(open_file[clss])
+			self.class2file[clss] = clss_example_list
+			self.clss2label[clss] = torch.LongTensor([i])
+
+		open_file.close()
+
+		self.n_classes = len(self.class2file)
+
+	def update_lists(self):
+
+		self.example_list = []
+
+		for i, clss in enumerate(self.class2file):
+			clss_file_list = np.random.permutation(self.class2file[spk])
+
+			idxs = strided_app(np.arange(len(clss_file_list)),5,5)
+
+			for idxs_list in idxs:
+				if len(idxs_list)==5:
+					self.example_list.append([clss_file_list[file_idx] for file_idx in idxs_list])
+					self.example_list[-1].append(clss)
+					self.example_list[-1].append(self.clss2label[clss])
+
+if __name__=='__main__':
+
+	import torch.utils.data
+	import argparse
+
+	parser = argparse.ArgumentParser(description='Test data loader')
+	parser.add_argument('--hdf-file', type=str, default='./data/train.hdf', metavar='Path', help='Path to hdf data')
+	args = parser.parse_args()
+
+	transform = transforms.Compose([transforms.ToPil(), transforms.RandomCrop(84, padding=4), transforms.RandomHorizontalFlip(), transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])	
+	dataset = Loader(hdf5_name=args.hdf_file, transform)
+	loader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=True, num_workers=4)
+
+	loader.dataset.update_lists()
+
+	print('Dataset length: {}, {}'.format(len(loader.dataset), len(loader.dataset.utt_list)))
+
+	spk2utt = {}
+
+	for batch in loader:
+		utt_1, utt_2, utt_3, utt_4, utt_5, spk, y = batch
+
+		for i in range(len(batch[-1])):
+			if spk[i] in spk2utt:
+				spk2utt[spk[i]]+=[utt_1[i], utt_2[i], utt_3[i], utt_4[i], utt_5[i]]
+			else:
+				spk2utt[spk[i]]=[utt_1[i], utt_2[i], utt_3[i], utt_4[i], utt_5[i]]
---
+++ mini_imagenet/data_prep.py
@@ -0,0 +1,42 @@
+import argparse
+import h5py
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import os
+import tqdm
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='Train data preparation and storage in .hdf')
+	parser.add_argument('--path-to-data', type=str, default='./data/', metavar='Path', help='Path to scp files with features')
+	parser.add_argument('--out-path', type=str, default='./', metavar='Path', help='Path to output hdf file')
+	parser.add_argument('--out-name', type=str, default='train.hdf', metavar='Path', help='Output hdf file name')
+	parser.add_argument('--min-examples', type=int, default=-1, help='Minimum number of examples per class')
+	args = parser.parse_args()
+
+	if os.path.isfile(args.out_path+args.out_name):
+		os.remove(args.out_path+args.out_name)
+		print(args.out_path+args.out_name+' Removed')
+
+	dataset = datasets.ImageFolder(args.data_path, transform=transforms)
+	dataloader = torch.utils.data.DataLoader(trainset, batch_size=1, shuffle=False, num_workers=args.n_workers)
+
+	print('Start of data preparation')
+
+	hdf = h5py.File(args.out_path+args.out_name, 'a')
+
+	data_iterator = tqdm(enumerate(dataloader), total=len(dataloader))
+
+	for i, data_label in data_iterator:
+
+		x, y = data_label
+		y = str(y.squeeze().item())
+
+		if not y in hdf:
+			hdf.create_group(spk)
+
+		hdf[y].create_dataset(y+'_'+str(i), data=features)
+
+	hdf.close()
