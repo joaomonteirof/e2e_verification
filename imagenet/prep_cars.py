1512bd30c3c0313200a3f55bac64c30cf798eb5e
joaomonteirof <joaomonteirof@gmail.com>
2020-01-29 22:38:01 -0500

    data prep for retrieval corpora

---
+++ imagenet/prep_cars.py
@@ -0,0 +1,48 @@
+import argparse
+import numpy as np
+import os
+from tqdm import tqdm
+import scipy.io as sio
+import pathlib
+import shutil
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='Train data preparation and storage in .hdf')
+	parser.add_argument('--path-to-data', type=str, default='./data/', metavar='Path', help='Path to scp files with features')
+	parser.add_argument('--path-to-metadata', type=str, default='./data/metada.mat', metavar='Path', help='Path to metadata')
+	parser.add_argument('--out-path', type=str, default='./', metavar='Path', help='Path to output hdf file')
+	parser.add_argument('--n-workers', type=int, default=4, metavar='N', help='Workers for data loading. Default is 4')
+	args = parser.parse_args()
+
+	metadata = sio.loadmat(args.path_to_metadata)
+	annotations = metadata['annotations']
+    annotations_labels = annotations['class']
+    unique_labels = np.sort(np.unique(annotations_labels))
+
+    train_labels, test_labels = unique_labels[:len(unique_labels)//2], unique_labels[len(unique_labels)//2:]
+
+	print('Start of data preparation')
+
+	classes = metadata['annotations']['class'][0]
+	paths = metadata['annotations']['fname'][0]
+
+	data_iterator = tqdm(enumerate(classes), total=len(classes))
+
+	for i, class_id in data_iterator:
+
+		if class_id in train_labels:
+			folder = 'train'
+		elif class_id in test_labels:
+			folder = 'test'
+		else:
+			print('\nClass id {} not found !!\n'.format(class_id))
+			continue
+
+		class_id = str(class_id[0][0])
+		source_path = os.path.join(args.path_to_data, paths[i][0])
+		tgt_path = os.path.join(args.out_path, folder, class_id, paths[i][0])
+
+		pathlib.Path(os.path.join(args.data_path, folder, class_id).mkdir(parents=True, exist_ok=True))
+
+		shutil.move(source_path, tgt_path)
---
+++ imagenet/prep_sop.py
@@ -0,0 +1,47 @@
+import argparse
+import numpy as np
+import os
+from tqdm import tqdm
+import scipy.io as sio
+import pathlib
+import shutil
+
+def read_metadata(path):
+	with open(path, 'r') as file:
+		info = file.readlines()
+
+	label_list, path_list = [], []
+
+	for line in utt_labels:
+		_, label, _, im = line.split(' ')
+		im = im.strip()
+		label_list.append(label)
+		path_list.append(im)
+
+	return label_list, path_list
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='Train data preparation and storage in .hdf')
+	parser.add_argument('--path-to-data', type=str, default='./data/', metavar='Path', help='Path to scp files with features')
+	parser.add_argument('--path-to-metadata', type=str, default='./data/metada.mat', metavar='Path', help='Path to metadata')
+	parser.add_argument('--out-path', type=str, default='./', metavar='Path', help='Path to output hdf file')
+	parser.add_argument('--n-workers', type=int, default=4, metavar='N', help='Workers for data loading. Default is 4')
+	args = parser.parse_args()
+
+	transform = transforms.Compose([transforms.ToTensor()])
+
+	label_list, path_list = read_metadata(args.path_to_metadata)
+
+	print('Start of data preparation')
+
+	data_iterator = tqdm(enumerate(label_list), total=len(label_list))
+
+	for i, class_id in data_iterator:
+
+		source_path = os.path.join(args.path_to_data, class_id, path_list[i])
+		tgt_path = os.path.join(args.out_path, class_id, path_list[i].split('/')[-1])
+
+		pathlib.Path(os.path.join(args.data_path, class_id).mkdir(parents=True, exist_ok=True))
+
+		shutil.move(source_path, tgt_path)
